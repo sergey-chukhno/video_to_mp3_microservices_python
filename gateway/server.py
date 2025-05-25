4b30ddd16f0c4c58a08b921d50b4616ba63ce05f
Sergey Chukhno <sergey.chukhno@laplateforme.io>
2025-05-25 16:15:55 +0200

    🔥 Gateway service
    Added login, upload and download endpoints

--- gateway/server.py
+++ gateway/server.py
@@ -19,3 +19,32 @@ channel = connection.channel()
 def login(): 
   token, err = access.login(request)
 
+  if not err: 
+    return token 
+  else: 
+    return err  
+
+@server.route("/upload", methods=["POST"])
+def upload(): 
+  access, err = validate.token(request)
+  access = json.loads(access)
+
+  if access["admin"]: 
+    if len(request.files) > 1 or len(request.files) < 1: 
+      return 'Exaclty one file required', 400 
+    for _, f in request.files.items(): 
+      err = util.upload(f, fs, channel, access)
+
+      if err: 
+        return err 
+    
+    return "Success!", 200
+  else: 
+    return "Unauthorized", 401
+  
+@server.route("/download", methods=["GET"])
+def download():
+  pass
+
+if __name__ == "__main__": 
+  server.run(host="0.0.0.0", port = 8080)
